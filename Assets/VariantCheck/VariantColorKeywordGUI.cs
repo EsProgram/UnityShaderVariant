17f1f00a54ddd5ecdaef64b901b4778af354f69c
Es <Email Address>
2016-03-16 22:36:18 +0900

    Variantテストの追加

---
+++ Assets/VariantCheck/VariantColorKeywordGUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VariantColorKeywordGUI : MonoBehaviour
+{
+  public void OnGUI()
+  {
+    KeywordButtonGUI("RED");
+    KeywordButtonGUI("GREEN");
+    KeywordButtonGUI("BLUE");
+  }
+
+  private void KeywordButtonGUI(string keyword)
+  {
+    if(GUILayout.Button(keyword + " : ON"))
+      Shader.EnableKeyword(keyword);
+    if(GUILayout.Button(keyword + " : OFF"))
+      Shader.DisableKeyword(keyword);
+    GUILayout.Label(keyword + " Enabled : " + Shader.IsKeywordEnabled(keyword).ToString());
+  }
+}
